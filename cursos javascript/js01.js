f868a64e1ff75569dc8898fdfcba306babd9305e
BReaer03 <gabrielpaez011@gmail.com>
2021-03-08 15:43:13 -0400

    cursos

---
+++ cursos javascript/js01.js
@@ -0,0 +1,17 @@
+alert("Welcome to my Web");
+
+//datos personales del ingresante
+let name; 
+let age;
+let citicen;
+
+name = prompt("¿Cual es tu nombre?");
+
+age = prompt("¿Cual es tu edad?");
+
+citicen = prompt("¿De donde eres?");
+
+alert("Hola " + name + ", bienvenido, procederemos a crea tu perfil.");
+
+//Creacion de base de datos de usuario
+
